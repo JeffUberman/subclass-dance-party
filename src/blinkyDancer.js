fa03dc429223bb11fc58fb8aba7ac7497d430166
MKS-students <mks.students@hackreactor.com>
2015-08-03 20:03:38 -0500

    Refactor code into Pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,35 @@
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.time = timeBetweenSteps;
+ // this.step();
+  this.setPosition(top, left);
+}
+ BlinkyDancer.prototype = new Dancer();
+
+ var oldStep = BlinkyDancer.prototype.step;
+
+ BlinkyDancer.prototype.step = function(){
+    this.$node.toggle();
+    Dancer.prototype.step.call(this);
+    // setTimeout(function(){console.log('blinky'); currThis.step();}, currTime);
+  };
+
+createBlinkyDancer = function(top, left, timeBetweenSteps){
+  var blinkyDancer = new BlinkyDancer(top, left, timeBetweenSteps);
+  return blinkyDancer;
+}
+
+
+
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
 
-  return blinkyDancer;
-};
--- src/init.js
+++ src/init.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
 
+
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
